084137ef3f2deb2be9d75c0ba974e4b1671630fd
Vinnie Magro <vmagro@socaldevs.com>
2014-10-11 14:23:32 -0700

    Card model and controller. Compile using handlebars

---
+++ api/controllers/CardController.js
@@ -0,0 +1,43 @@
+/**
+ * CardController
+ *
+ * @module      :: Controller
+ * @description    :: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+var handlebars = require('handlebars');
+
+module.exports = {
+
+    preview: function (req, res) {
+        compileCard(req.params.id, function (html) {
+            return res.send(html);
+        });
+    },
+
+    /**
+     * Overrides for the settings in `config/controllers.js`
+     * (specific to CardController)
+     */
+    _config: {}
+
+
+};
+
+function compileCard(cardId, callback) {
+    Card.findOne({id: cardId}).exec(function (err, card) {
+        Template.findOne({id: card.templateId}).exec(function (err, template) {
+            callback(handlebars.compile(template.html)(card.variables));
+        });
+    });
+}
---
+++ api/models/Card.js
@@ -0,0 +1,18 @@
+/**
+ * Card
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    attributes: {
+
+        templateId: 'string',
+        variables: 'json'
+
+    }
+
+};
