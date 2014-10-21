b4f451e3668fa4013c5cebd4ea9e9a2033e330ef
Vinnie Magro <vmagro@socaldevs.com>
2014-10-21 16:47:33 -0700

    Revert "Switch to mongohq"
    This reverts commit 159cc2ad8743fc32b95edb00d5e48df861397034.

--- config/connections.js
+++ config/connections.js
@@ -1,6 +1,6 @@
 module.exports.connections = {
-    productionMongoHQ: {
-        adapter: 'sails-mongo',
-        url: process.env.MONGOHQ_URL
-    }
+  productionHerokuPostgres: {
+    adapter: 'sails-postgresql',
+    url: process.env.DATABASE_URL
+  }
 };
--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
     models: {
-        connection: 'productionMongoHQ'
+        connection: 'productionHerokuPostgres'
     }
-};
+}
