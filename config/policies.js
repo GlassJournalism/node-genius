718377162b38900e574e2766ef73e858a2fb0938
Vinnie Magro <vmagro@socaldevs.com>
2014-10-28 19:17:50 -0700

    Require login for everything but homepage

--- config/policies.js
+++ config/policies.js
@@ -16,11 +16,9 @@ module.exports.policies = {
 
     // Default policy for all controllers and actions
     // (`true` allows public access)
-    '*': true,
+    '*': 'isAuthenticated',
 
-    CardController: {
-        '*': 'isAuthenticated'
-    }
+    '/': true
 
     /*
      // Here's an example of adding some policies to a controller
