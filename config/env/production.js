4449ac85f7edf20d421cb946a42958728b90dfb2
Vinnie Magro <vmagro@socaldevs.com>
2014-12-05 23:52:07 -0800

    Hopefully use redis in production

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,20 @@
 module.exports = {
     models: {
         connection: 'productionMongoHQ'
+    },
+
+    session: {
+        adapter: 'redis',
+        //
+        // The following values are optional, if no options are set a redis instance running
+        // on localhost is expected.
+        // Read more about options at: https://github.com/visionmedia/connect-redis
+        //
+        host: 'tarpon.redistogo.com',
+        port: 9958,
+//  ttl: <redis session TTL in seconds>,
+        db: 'redistogo',
+        pass: 'a641d4d4e2090099a63bf22430aea054',
+        prefix: 'sess:'
     }
 };
